2ea3d2715c662b8e825faffaaa440c1a10a362c7
yusuf-multhan <yusuf-multhan@users.noreply.github.com>
2020-09-01 15:41:53 +0530

    Initial clone from master

--- server.js
+++ server.js
@@ -54,18 +54,18 @@ server.listen(port, () => console.log(`Running on localhost:${port}`));
     if(!Number.prototype.noExponents) {
         Number.prototype.noExponents= function(){
             var data= String(this).split(/[eE]/);
-            if(data.length== 1) return data[0]; 
-        
+            if(data.length== 1) return data[0];
+
             var  z= '', sign= this<0? '-':'',
             str= data[0].replace('.', ''),
             mag= Number(data[1])+ 1;
-        
+
             if(mag<0){
                 z= sign + '0.';
                 while(mag++) z += '0';
                 return z + str.replace(/^\-/,'');
             }
-            mag -= str.length;  
+            mag -= str.length;
             while(mag--) z += '0';
             return str + z;
         }
